04cdecc8dc5e37c37dee45a101d444111a85d363
sparkweb <sparkweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-23 07:05:18 +0000

    Initial Commit
    git-svn-id: https://plugins.svn.wordpress.org/hide-youtube-related-videos/trunk@479691 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hide-youtube-related-videos.php
@@ -0,0 +1,54 @@
+<?php /*
+**************************************************************************
+Plugin Name: Hide YouTube Related Videos
+Plugin URI: http://wordpress.org/extend/plugins/hide-youtube-related-videos/
+Description: This is a simple plugin to keep the YouTube oEmbed from showing related videos.
+Author: SparkWeb Interactive, Inc.
+Version: 1.0
+Author URI: http://www.soapboxdave.com/
+
+**************************************************************************
+
+Copyright (C) 2012 SparkWeb Interactive, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+**************************************************************************/
+
+//The Filter That Does the Work
+add_filter( 'oembed_result', 'hide_youtube_related_videos', 10, 3);
+function hide_youtube_related_videos($data, $url, $args = array()) {
+	return str_replace('&feature=oembed', '&feature=oembed&rel=0', $data);
+}
+
+
+//On Activation, all oembed caches are cleared 
+register_activation_hook(__FILE__, 'hide_youtube_related_videos_activation');
+function hide_youtube_related_videos_activation() {
+	global $wpdb; 
+	
+	$post_ids = $wpdb->get_col( "SELECT DISTINCT post_id FROM $wpdb->postmeta WHERE meta_key LIKE '_oembed_%'" ); 
+ 	if ( $post_ids ) { 
+ 		$postmetaids = $wpdb->get_col( "SELECT meta_id FROM $wpdb->postmeta WHERE meta_key LIKE '_oembed_%'" ); 
+ 		$in = implode( ',', array_fill( 1, count($postmetaids), '%d' ) );  
+ 		do_action( 'delete_postmeta', $postmetaids ); 
+ 		$wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->postmeta WHERE meta_id IN($in)", $postmetaids ) );  
+ 		do_action( 'deleted_postmeta', $postmetaids ); 
+ 		foreach ( $post_ids as $post_id ) 
+ 			wp_cache_delete( $post_id, 'post_meta' ); 
+ 		return true; 
+ 	} 
+
+}
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Hide YouTube Related Videos ===
+Contributors: sparkweb
+Tags: youtube, video, oembed, related
+Requires at least: 2.9
+Tested up to: 3.3
+Stable tag: 1.0
+This is a simple plugin to keep the YouTube oEmbed from showing related videos.
+
+== Description ==
+
+WordPress' built-in oEmbed feature is fantastic. I've heard several complaints, though, about the related videos that show up after the video is done playing so I put together this very simple plugin that keeps the YouTube oEmbed code from showing related videos.
+
+On activation, the plugin clears the oEmbed cache so that the videos can be successfully re-cached with the new setting.
+
+== Installation ==
+
+Copy the folder to your WordPress 
+'*/wp-content/plugins/*' folder.
+
+1. Activate the plugin in your WordPress admin
+1. That's it. There's nothing else to do. Really!
+
+
+
+== Changelog ==
+
+= 1.0 (Dec 23, 2011) =
+* Initial Release
